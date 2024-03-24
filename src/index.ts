71303a97c902ccd9dfa26fe82d83efadbcb4840e
root <root@LAPTOP-504KTS8O>
2024-03-24 23:53:20 +0530

    docker init`

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello World!..' });
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
